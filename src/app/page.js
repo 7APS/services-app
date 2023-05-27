6e8ca9a552b40d4f4164f853113929d6e48083de
Luiz Casara <luiz.casara@maxxidata.com>
2023-05-27 17:36:52 -0300

    fix '/' problem

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 "use client"
 
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter();
-  router.push('/signin');
+
+  useEffect(() => {
+    router.push('/signin');
+  }, []);
 }
