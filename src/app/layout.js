76ca24485c1c64e6dface451c6de7edc4f43550e
Luiz Casara <luiz.casara@maxxidata.com>
2023-08-03 21:35:28 -0300

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,21 @@
-import { ConfigProvider } from 'antd';
-import '../../input.css';
-import 'antd/dist/reset.css';
-import 'devextreme/dist/css/dx.light.css';
+import Analytics from "@/components/Analytics";
+import "devextreme/dist/css/dx.light.css";
+import "antd/dist/reset.css";
+import "../../input.css";
 
 export const metadata = {
-    title: 'SOUPE APP',
-    description: 'Soupe app',
-}
+  title: "SOUPE APP",
+  description: "Soupe app",
+};
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="pt-BR">
-            <body>
-                <h1 className='hidden'>layout Root</h1>
-                {children}
-            </body>
-        </html>
-    )
+  return (
+    <html lang="pt-BR">
+      <body>
+        <Analytics />
+        <h1 className="hidden">layout Root</h1>
+        {children}
+      </body>
+    </html>
+  );
 }
